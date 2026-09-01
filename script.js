3fb6e1fcd10ed5479171c4139f6463650b747d97
shaheereminent <shaheer_eminent@hotmail.com>
2026-09-01 18:46:01 +0500

    feature: idk forgot

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 'use strict';
 
-// selecting start guessing element
-let messageElement = document.querySelector('.message');
-console.log(messageElement.textContent);
-
-// selecting secret number
-let secretNumberElement = document.querySelector('.number');
-console.log(secretNumberElement.textContent);
-
-// selecting score element
-let scoreElement = document.querySelector('.score');
-console.log(scoreElement.textContent);
-
 // generating number secret number
 const randomSecretNumber = Math.trunc(Math.random() * 20 + 1);
 console.log(randomSecretNumber);
+
+// selecting start guessing element
+let messageElement = document.querySelector('.message');
+
+// selecting highScore element
+let highScoreElement = document.querySelector('.highscore');
+
+// selecting secret number
+let secretNumberElement = document.querySelector('.number');
+
+// selecting score element
+let scoreElement = document.querySelector('.score');
+
+// selecting guess input element
+let guessInputElement = document.querySelector('.guess');
+
+// clicking check button check
+document.querySelector('.check').addEventListener('click', function () {
+  const userGuess = Number(guessInputElement.value);
+
+  if (!userGuess) {
+    messageElement.textContent = '⛔ No Number!';
+  }
+
+  if (userGuess === randomSecretNumber) {
+    messageElement.textContent = '🎉 Correct Number!';
+    secretNumberElement.textContent = randomSecretNumber;
+    if (
+      Number(scoreElement.textContent) > Number(highScoreElement.textContent)
+    ) {
+      highScoreElement.textContent = scoreElement.textContent;
+    }
+  } else if (userGuess !== randomSecretNumber) {
+    scoreElement.textContent -= 1;
+  }
+});
