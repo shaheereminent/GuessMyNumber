1eba4ec133fbeeac23875ca5c8c387330ee0cc37
shaheereminent <shaheer_eminent@hotmail.com>
2026-09-02 09:25:26 +0500

    feat: add high score capability, add low score count capability, add again game reset button function, add background color on correct answer

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 'use strict';
 
 // generating number secret number
-const randomSecretNumber = Math.trunc(Math.random() * 20 + 1);
+let randomSecretNumber = Math.trunc(Math.random() * 20 + 1);
 console.log(randomSecretNumber);
+
+// selecting entire body background color
+let backgroundColor = document.querySelector('body');
 
 // selecting start guessing element
 let messageElement = document.querySelector('.message');
@@ -22,7 +25,10 @@ let guessInputElement = document.querySelector('.guess');
 // creating score variable to keep track of it
 let score = 20;
 
-// clicking check button check
+// getting players name
+// prompt('Enter your name: ');
+
+// clicking check button to check user guessed value
 document.querySelector('.check').addEventListener('click', function () {
   const userGuess = Number(guessInputElement.value);
 
@@ -35,6 +41,8 @@ document.querySelector('.check').addEventListener('click', function () {
   if (userGuess === randomSecretNumber) {
     messageElement.textContent = '🎉 Correct Number!';
     secretNumberElement.textContent = randomSecretNumber;
+    backgroundColor.style.backgroundColor = '#60b347';
+    secretNumberElement.style.width = '30rem';
     // checking if user current score is higher than previous score
     if (
       Number(scoreElement.textContent) > Number(highScoreElement.textContent)
@@ -42,15 +50,40 @@ document.querySelector('.check').addEventListener('click', function () {
       highScoreElement.textContent = scoreElement.textContent;
     }
   } else if (userGuess > randomSecretNumber) {
-    messageElement.textContent = '📈 Too high!';
-    score--;
-    scoreElement.textContent = score;
+    if (score > 1) {
+      messageElement.textContent = '📈 Too high!';
+      score--;
+      scoreElement.textContent = score;
+    } else {
+      messageElement.textContent = '💥 You lost the game!';
+      score = 0;
+      scoreElement.textContent = score;
+    }
   } else if (userGuess < randomSecretNumber) {
-    messageElement.textContent = '📉 Too low!';
-    score--;
-    scoreElement.textContent = score;
+    if (score > 1) {
+      messageElement.textContent = '📉 Too low!';
+      score--;
+      scoreElement.textContent = score;
+    } else {
+      messageElement.textContent = '💥 You lost the game!';
+      score = 0;
+      scoreElement.textContent = score;
+    }
   } else if (userGuess !== randomSecretNumber) {
     score--;
     scoreElement.textContent = score;
   }
+});
+
+// clicking again button to reset the game state
+document.querySelector('.again').addEventListener('click', function () {
+  // resetting message for the user
+  messageElement.textContent = 'Start guessing...';
+  // reset score state value and in dom as well
+  score = 20;
+  scoreElement.textContent = score;
+  // re-initializing a new random number
+  randomSecretNumber = Math.trunc(Math.random() * 20 + 1);
+  // resetting secret number back to a "question mark"
+  secretNumberElement.textContent = '?';
 });
